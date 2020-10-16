2ba92b702c6abc1c158a8fbb5400f8ec0ce9f3e2
Philip Purwoko <Philippurwoko123@gmail.com>
2020-10-16 09:38:25 +0700

    Menggunakan semantic html, merapikan kode, menambahkan comment, dan menambahkan informasi pada package.json

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Simple, fast, and responsive syntax highlighter app. Developed with vue. Deploye
 
 | Step by Step                             | Command       |
 |------------------------------------------|---------------|
-| Install packages                         | `npm install`   |
-| Compiles and hot-reloads for development | `npm run serve` |
-| Compiles and minifies for production     | `npm run build` |
-| Lints and fixes files                    | `npm run lint`  |
+| Install packages                         | npm install   |
+| Compiles and hot-reloads for development | npm run serve |
+| Compiles and minifies for production     | npm run build |
+| Lints and fixes files                    | npm run lint  |
--- src/components/CodeBlock.vue
+++ src/components/CodeBlock.vue
@@ -7,10 +7,10 @@
                 <option v-bind:key="lan" v-for="lan in getLangs">{{ lan }}</option>
             </select>
         </form>
-        <section class="code-container">
-            <textarea class="code-block" v-model="code" @input="highlight" ></textarea>
+        <main class="code-container">
+            <textarea class="code-block" v-model="raw" @input="highlight" ></textarea>
             <div class="code-block formated" v-html="formated"></div>
-        </section>
+        </main>
     </div>
 </template>
 
@@ -19,15 +19,16 @@
     import { mapGetters  } from 'vuex';
 
     export default {
-        data: function() {
+        data() {
             return {
                 lang: "",
                 languages:[],
-                code: "",
+                raw: "",
                 formated: ""
             };
         },
         computed:{
+            // Using vuex getters
             ...mapGetters([
                 'getAPI',
                 'getLangAPI'
@@ -37,6 +38,7 @@
             }
         },
         created(){
+            // Get list of programming languages on created
             axios.get(this.getLangAPI).then(res=>{
                 res.data.data.languages.forEach(lang=>{
                     this.languages.push(lang);
@@ -45,20 +47,15 @@
         },
         methods: {
             highlight() {
-                if (this.code == "") {
-                    this.code = " ";
+                // If code is empty, replace with space
+                if (this.raw == "") {
+                    this.raw = " ";
                 }
+                // If there is no language selected
                 if (this.lang != "") {
                     axios
-                        .post(
-                            this.getAPI + `?lang=${this.lang}`,
-                            {
-                                code: this.code
-                            }
-                        )
-                        .then(res => {
-                            this.formated = res.data.data;
-                        });
+                        .post(this.getAPI + `?lang=${this.lang}`,{code: this.raw})
+                        .then(res => {this.formated = res.data.data;});
                 } else {
                     this.formated = "<p class='monospace' style='color:azure;'>No language selected. Please select a language</p>";
                 }
@@ -68,11 +65,13 @@
 </script>
 
 <style scoped>
+
     .code-container {
         display: flex;
         flex-direction: row;
         height: 400px;
     }
+
     .code-block {
         flex: 50%;
         overflow: scroll;
@@ -80,21 +79,27 @@
         border: 2px solid black;
         padding: 10px;
     }
+
     textarea {
         resize: none;
     }
+
     .code-block:focus {
         outline: none !important;
         border: 2px solid darkorange;
     }
+
     .formated {
         background-color: rgb(0, 50, 60);
     }
+
+    /* Flexbox Code Block */
     @media (max-width: 800px) {
         .code-container {
             flex-direction: column;
         }
     }
+
     select {
         border-radius: 5px;
         padding: 4px;
@@ -102,32 +107,40 @@
         margin-top: 5px;
         font-family: "Share Tech Mono", monospace;
     }
+
     select:focus {
         background-color: #eee;
     }
+
     form {
         width: 250px;
         margin: auto;
         text-align: center;
     }
 
+    /* Scrollbar CSS */
     ::-webkit-scrollbar {
         width: 8px;
         height: 8px;
     }
+
     ::-webkit-scrollbar-track {
         background: none;
         box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
         -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
     }
+
     ::-webkit-scrollbar-thumb {
         background: #888;
         border-radius: 5px;
     }
+
     ::-webkit-scrollbar-thumb:hover {
         background: #555;
     }
+
     ::-webkit-scrollbar-corner {
         background: none;
     }
+
 </style>
