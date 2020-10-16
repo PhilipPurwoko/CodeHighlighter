7ed2583c842a92e835030ec939af64ca3c510d6d
Philip Purwoko <Philippurwoko123@gmail.com>
2020-10-16 09:38:49 +0700

    Menambahkan pop up notifikasi

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,31 +1,73 @@
 <template>
     <header>
-        <h1>{{ getTitle }}</h1>
+        <h1 @mouseover="toogleToast" @mouseleave="toogleToast" class="title center">{{ getTitle }}</h1>
+        <article class="toast center monospace" v-bind:class="{hovering:hovering}">
+            <strong>Get your code highlighted instantly</strong>
+            <p>Simply select your programming language then copy and paste your code here</p>
+        </article>
     </header>
 </template>
 
 <script>
+    // Using vuex getters
     import { mapGetters } from 'vuex';
     export default {
+        data(){
+            return{
+                hovering:false
+            }
+        },
         computed:{
             ...mapGetters([
                 'getTitle'
             ])
+        },
+        methods:{
+            toogleToast(){
+                this.hovering = !this.hovering;
+            }
         }
     };
 </script>
 
 <style scoped>
+
     header{
         text-align: center;
-        font-family: 'Righteous', cursive;
-        background-color: #006a71;
+    }
+
+    .center{
+        left: 0; 
+        right: 0; 
+        margin-left: auto; 
+        margin-right: auto;
+    }
+
+    .title{
         width: 300px;
-        margin: auto;
         border-radius: 15px;
-    }
-    h1{
-        padding: 10px;
+        background-color: #006a71;
         color: azure;
+        font-family: 'Righteous', cursive;
+        padding: 10px;
     }
+    
+    .toast{
+        z-index: -1;
+        padding: 10px;
+        border-radius: 3px;
+        background-color: rgb(250, 250, 250);
+        color: #333;
+        width: 300px;
+        position: absolute;
+        margin-top: -10px;
+        transition: all 0.3s;
+        opacity: 0;
+    }
+
+    .hovering{
+        z-index: 1;
+        opacity: 0.95;
+    }
+
 </style>
